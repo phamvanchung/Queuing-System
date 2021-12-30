adf144612ffe7acf1dd782c49c4b1a5a3e3bea90
Chung Phạm Văn <chung14799@gmail.com>
2021-12-30 22:34:29 +0700

    first commit

---
+++ src/pages/Auth/ForgotPass/index.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const index = (props: Props) => {
+  return <div></div>;
+};
+
+export default index;
---
+++ src/pages/Auth/LoginPage/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const Login = (props: Props) => {
+  return <div>thí í login</div>;
+};
+
+export default Login;
