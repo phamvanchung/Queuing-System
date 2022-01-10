98ec519dfe4b45199a9f2a3baf43438500b80990
Chung Phạm Văn <chung14799@gmail.com>
2022-01-10 17:28:19 +0700

    add not-found-page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,9 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.scss";
 
-interface Props {}
-
-const NotFound = (props: Props) => {
-  return <div>this is NotFound</div>;
+const NotFound = () => {
+  return (
+    <div className="root-nf">
+      <div className="moon" />
+      <div className="moon__crater moon__crater1" />
+      <div className="moon__crater moon__crater2" />
+      <div className="moon__crater moon__crater3" />
+      <div className="star star1" />
+      <div className="star star2" />
+      <div className="star star3" />
+      <div className="star star4" />
+      <div className="star star5" />
+      <div className="error">
+        <div className="error__title">404</div>
+        <div className="error__subtitle">Hmmm...</div>
+        <div className="error__description">
+          It looks like one of the developers fell asleep
+        </div>
+        <Link to="/" className="error__button error__button--active">
+          Home Page
+        </Link>
+      </div>
+      <div className="astronaut">
+        <div className="astronaut__backpack" />
+        <div className="astronaut__body" />
+        <div className="astronaut__body__chest" />
+        <div className="astronaut__arm-left1" />
+        <div className="astronaut__arm-left2" />
+        <div className="astronaut__arm-right1" />
+        <div className="astronaut__arm-right2" />
+        <div className="astronaut__arm-thumb-left" />
+        <div className="astronaut__arm-thumb-right" />
+        <div className="astronaut__leg-left" />
+        <div className="astronaut__leg-right" />
+        <div className="astronaut__foot-left" />
+        <div className="astronaut__foot-right" />
+        <div className="astronaut__wrist-left" />
+        <div className="astronaut__wrist-right" />
+        <div className="astronaut__cord">
+          <canvas id="cord" height="500px" width="500px" />
+        </div>
+        <div className="astronaut__head">
+          <canvas id="visor" width="60px" height="60px" />
+          <div className="astronaut__head-visor-flare1" />
+          <div className="astronaut__head-visor-flare2" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
